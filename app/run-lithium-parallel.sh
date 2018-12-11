80c92c03959806e821b7783b9f9c0c24db8b90c5
sofiaoreis <sofiareis1994@gmail.com>
2018-12-11 19:14:50 +0000

    missing files

--- app/run-lithium-parallel.sh
+++ app/run-lithium-parallel.sh
@@ -2,13 +2,14 @@
 
 PROJECT=$1
 BUGS=$2
+TOP=$3
 
 IFS="," read -ra ADDR <<< "$BUGS"
 
 if [[ ${#ADDR[@]} > 1 ]]; then BUGS_LIST="${ADDR[@]}"; else BUGS_LIST=$(seq 1 $BUGS); fi
 
 for BUG in $BUGS_LIST; do
-	bash runner.sh $PROJECT $BUG &
+	bash runner.sh $PROJECT $BUG $TOP&
 done
 
 wait
